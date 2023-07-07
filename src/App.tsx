b72eb6f71f1af5471c5672234a86cbf584576ef3
Anthony Mclamb <adylanmclamb@yahoo.com>
2023-07-07 12:50:51 -0400

    Testing

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,7 @@
 import Layout from "./layout/Layout";
-import PageRoutes from "./pages/PageRoutes";
 
 const App = () => {
-  return (
-    <Layout>
-      <PageRoutes />
-    </Layout>
-  );
+  return <Layout></Layout>;
 };
 
 export default App;
