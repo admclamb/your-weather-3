b3e3913b7a8cb0ac8c946a6ccb82e64eca8f83ec
Anthony Mclamb <adylanmclamb@yahoo.com>
2023-07-08 21:16:36 -0400

    setup dropdown

--- src/components/Searchbar/Searchbar.tsx
+++ src/components/Searchbar/Searchbar.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import FormInput from "../Form/FormInput/FormInput";
 import Button from "../Button/Button";
 import { useNavigate } from "react-router-dom";
+import SearchbarDropdown from "./SearchbarDropdown/SearchbarDropdown";
 
 type Props = {
   className?: string;
@@ -9,6 +10,7 @@ type Props = {
 
 const Searchbar = ({ className = "" }: Props) => {
   const [input, setInput] = useState<string>("");
+  const [isDropdownOpen, setIsDropdownOpen] = useState<boolean>(false);
   const navigate = useNavigate();
 
   const submitSearch = async (event: React.FormEvent) => {
@@ -17,24 +19,32 @@ const Searchbar = ({ className = "" }: Props) => {
     navigate("search-locations", { state: { search: input } });
   };
   return (
-    <form
-      onSubmit={submitSearch}
-      className={`relative text-black${className && " " + className}`}
-    >
-      <FormInput
-        input={input}
-        setInput={setInput}
-        className="w-full pr-12"
-        placeholder="Search..."
-        padding="py-1.5 pl-2 pr-12"
-      />
-      <Button
-        type="submit"
-        className="absolute right-0 top-1/2 -translate-y-1/2"
+    <div>
+      <form
+        onSubmit={submitSearch}
+        className={`relative text-black${className && " " + className}`}
       >
-        <i className="fa-solid fa-magnifying-glass"></i>
-      </Button>
-    </form>
+        <FormInput
+          input={input}
+          setInput={setInput}
+          className={`w-full ${isDropdownOpen ? "rounded-t" : "rounded"}`}
+          placeholder="Search..."
+          padding="p-3 pr-12"
+          onFocus={() => setIsDropdownOpen(true)}
+          onBlur={() => setIsDropdownOpen(false)}
+        />
+        <Button
+          type="submit"
+          className="absolute right-0 top-1/2 -translate-y-1/2"
+        >
+          <i className="fa-solid fa-magnifying-glass"></i>
+        </Button>
+      </form>
+      <SearchbarDropdown
+        isDropdownOpen={isDropdownOpen}
+        setIsDropdownOpen={setIsDropdownOpen}
+      />
+    </div>
   );
 };
 
