fd81b456dc4f0f34ad82d2c6b54eeca515e48a6a
Anthony Mclamb <adylanmclamb@yahoo.com>
2023-07-09 00:16:16 -0400

    Update types on api request

--- src/api/OpenWeather.ts
+++ src/api/OpenWeather.ts
@@ -1,7 +1,13 @@
+import { AxiosResponse } from "axios";
 import { parseLocation } from "../utils/parseLocation";
 import Api from "./Api";
+import { Weather } from "../ts/types/Weather";
+import { Location } from "../ts/types/Location";
 const API_KEY = import.meta.env.VITE_OW_KEY ?? "";
-const getLocationByName = async (location: string) => {
+
+const getLocationByName = async (
+  location: string
+): Promise<AxiosResponse<Location[]> | void> => {
   const {
     city = "",
     stateCode = "",
@@ -12,6 +18,16 @@ const getLocationByName = async (location: string) => {
       countryCode && `, ${countryCode}`
     }&&appid=${API_KEY}`
   );
+};
+
+export const getWeather = async (
+  lat: number,
+  lon: number,
+  units: string
+): Promise<AxiosResponse<Weather> | void> => {
+  return await Api.get<Weather>(
+    `https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&units=${units}&appid=${API_KEY}`
+  ).catch((err) => console.log(err));
 };
 
 const OpenWeather = {
